7363ead989efac747c8c55f55b5181c9e15cb85d
Ray Sakai <ray.sakai@logical-fabrics.com>
2015-12-21 01:42:03 +0900

    Added React.js base

---
+++ public/javascripts/components/FrontPage.js
@@ -0,0 +1,13 @@
+'use strict';
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+
+export default React.createClass({
+  render() {
+    return (
+      <Grid>
+        <div>I am FrontPage</div>
+      </Grid>
+    )
+  }
+})
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 const router  = require('express').Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'nborg' });
 });
 
 module.exports = router;
