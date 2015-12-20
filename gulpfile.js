24c54fbbd1effbfcbe7285e93fcd4104f1c8737f
Ray Sakai <ray.sakai@logical-fabrics.com>
2015-12-20 22:40:07 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp       = require('gulp');
+const supervisor = require('gulp-supervisor');
+const plumber    = require('gulp-plumber');
+const webpack    = require('webpack-stream');
+
+gulp.task('default', ['server', 'webpack'])
+
+gulp.task('server', () => {
+  supervisor('./bin/www', {
+    ignore: ['./public']
+  })
+});
+
+gulp.task('webpack', () => {
+  return gulp.src('public/javascripts/main.js')
+    .pipe(plumber())
+    .pipe(webpack({
+      watch: true,
+      output: { filename: 'bundle.js' },
+      module: {
+        loaders: [{
+          loader: 'babel',
+          exclude: /node_modules/,
+          test: /\.js[x]?$/,
+          query: {
+            cacheDirectory: true,
+            presets: ['react', 'es2015']
+          }
+        }],
+      }
+    }))
+    .pipe(gulp.dest('public/javascripts/'))
+});
