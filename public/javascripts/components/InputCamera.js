e92ece2cd1932a7370a4866d5d202443c1e87112
Ray Sakai <ray.sakai@logical-fabrics.com>
2015-12-24 19:44:37 +0900

    Repeat button

--- public/javascripts/components/InputCamera.js
+++ public/javascripts/components/InputCamera.js
@@ -2,24 +2,50 @@
 import React from 'react';
 import Reflux from 'reflux';
 import Webcam from 'react-webcam';
-import {Button} from 'react-bootstrap';
+import {ButtonToolbar, Button} from 'react-bootstrap';
 import AnalysisAction from '../actions/AnalysisAction';
 import AnalysisStore from '../stores/AnalysisStore';
+
+const REPEAT_INTERVAL = 5000;
 
 export default React.createClass({
   mixins: [Reflux.connect(AnalysisStore, 'analysis')],
 
-  onCapture() {
+  getInitialState() {
+    return {repeating: false}
+  },
+
+  onCaptureOne() {
     AnalysisAction.submit(this.refs.webcam.getScreenshot());
+  },
+
+  onCaptureRepeat() {
+    AnalysisAction.submit(this.refs.webcam.getScreenshot());
+    this.setState({
+      repeating: setInterval(this.onCaptureOne, REPEAT_INTERVAL)
+    });
+  },
+
+  onStopRepeat() {
+    this.setState({repeating: false});
+    clearInterval(this.state.repeating)
   },
 
   render() {
     return (
       <div>
         <Webcam width={320} height={240} audio={false} ref='webcam' screenshotFormat='image/jpeg'/>
-        <div style={{marginTop: 10}}>
-          <Button bsStyle="primary" onClick={this.onCapture}>Capture</Button>
-        </div>
+        <ButtonToolbar style={{marginTop: 10}}>
+          <Button bsStyle="primary" onClick={this.onCaptureOne}>Capture One</Button>
+          {(() => {
+            if(this.state.repeating) {
+              return <Button bsStyle="warning" onClick={this.onStopRepeat}>Stop</Button>
+            }
+            else {
+              return <Button bsStyle="success" onClick={this.onCaptureRepeat}>Every {REPEAT_INTERVAL / 1000} sec</Button>
+            }
+          })()}
+        </ButtonToolbar>
       </div>
     )
   }
